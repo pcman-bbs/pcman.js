bedc4b8df1489fe36360737826bf243d23ff5f2f
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-07-31 23:57:37 +0800

    Test case for escape

--- test/spec-ansiparser.js
+++ test/spec-ansiparser.js
@@ -6,23 +6,103 @@ import AnsiParser from '../lib/ansiparser';
 const assert = chai.assert;
 
 describe('AnsiParser', () => {
-	describe('normal', () => {
-		let termbuf = { puts: () => {} };
-		let spy = sinon.spy(termbuf, 'puts');
+	let termbuf;
+	let spy;
 
-		it('input ASCII', (done) => {
+	beforeEach(() => {
+		termbuf = {
+			puts: () => {},
+			scroll: () => {},
+			lineFeed: () => {},
+			carriageReturn: () => {},
+			saveCursor: () => {},
+			restoreCursor: () => {},
+		};
+		spy = {};
+
+		for (let name of Object.keys(termbuf)) {
+			spy[name] = sinon.spy(termbuf, name);
+		}
+	});
+
+	describe('normal', () => {
+		it('ASCII', (done) => {
 			const input = 'This is a test ascii string.';
 			const expected = input;
 
 			let parser = new AnsiParser(termbuf);
 			parser.feed(input);
 
-			assert.ok(spy.calledOnce);
-			assert.strictEqual(spy.getCall(0).args[0], expected);
+			assert.ok(spy.puts.calledOnce);
+			assert.strictEqual(spy.puts.getCall(0).args[0], expected);
+
+			done();
+		});
+	});
+
+	describe('ESC', () => {
+		it ('scroll up', (done) => {
+			const input = '\x1bD';
+
+			let parser = new AnsiParser(termbuf);
+			parser.feed(input);
+
+			assert.ok(spy.scroll.calledOnce);
+			assert.strictEqual(spy.scroll.getCall(0).args[0], false);
+			assert.strictEqual(spy.scroll.getCall(0).args[1], 1);
 
 			done();
 		});
 
+		it ('scroll down', (done) => {
+			const input = '\x1bM';
+
+			let parser = new AnsiParser(termbuf);
+			parser.feed(input);
+
+			assert.ok(spy.scroll.calledOnce);
+			assert.strictEqual(spy.scroll.getCall(0).args[0], true);
+			assert.strictEqual(spy.scroll.getCall(0).args[1], 1);
+
+			done();
+		});
+
+		it ('CR/LF', (done) => {
+			const input = '\x1bE';
+
+			let parser = new AnsiParser(termbuf);
+			parser.feed(input);
+
+			// FIXME: How to test function call order?
+			assert.ok(spy.lineFeed.calledOnce);
+			assert.ok(spy.carriageReturn.calledOnce);
+
+			done();
+		});
+
+		// it ('save cursor', (done) => {
+		// 	const input = '\x1b7';
+
+		// 	let parser = new AnsiParser(termbuf);
+		// 	parser.feed(input);
+
+		// 	console.log(spy.saveCursor.callCount);
+
+		// 	assert.ok(spy.saveCursor.calledOnce);
+
+		// 	done();
+		// });
+
+		// it ('restore cursor', (done) => {
+		// 	const input = '\x1b8';
+
+		// 	let parser = new AnsiParser(termbuf);
+		// 	parser.feed(input);
+
+		// 	assert.ok(spy.restoreCursor.calledOnce);
+
+		// 	done();
+		// });
 	});
 
 	describe('error', () => {
