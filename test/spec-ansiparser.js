8df4c75447bdca452d2cf9e65c7bc5804fbf5813
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-07-31 23:20:37 +0800

    Add test case for ansiparser

--- test/spec-ansiparser.js
+++ test/spec-ansiparser.js
@@ -1,4 +1,5 @@
 import chai from 'chai';
+import sinon from 'sinon';
 
 import AnsiParser from '../lib/ansiparser';
 
@@ -6,6 +7,21 @@ const assert = chai.assert;
 
 describe('AnsiParser', () => {
 	describe('normal', () => {
+		it('input ASCII', (done) => {
+			let termbuf = { puts: () => {} };
+			let spy = sinon.spy(termbuf, 'puts');
+
+			const input = 'This is a test ascii string.';
+			const expected = input;
+
+			let parser = new AnsiParser(termbuf);
+			parser.feed(input);
+
+			assert.ok(spy.calledOnce);
+			assert.strictEqual(spy.getCall(0).args[0], expected);
+
+			done();
+		});
 
 	});
 
