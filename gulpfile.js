42f717dbb99ee16e01d1163ec3ec38416558c7cd
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2015-07-31 22:33:34 +0800

    Use babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var babel = require('gulp-babel');
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 
@@ -7,8 +8,18 @@ var src = [
 	'lib/**/*.js',
 ];
 
+var dest = 'build';
+
+gulp.task('babel', function () {
+	return gulp.src(src)
+		.pipe(babel())
+		.pipe(gulp.dest(dest));
+});
+
 gulp.task('jshint', function () {
 	return gulp.src(src)
 		.pipe(jshint('.jshintrc'))
 		.pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('prepublish', ['babel']);
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 'use strict';
 
-module.exports = require('./lib/pcman.js');
+module.exports = require('./build/pcman.js');
